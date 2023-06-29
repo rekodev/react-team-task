a6a59ae73fdc2c6cc422c439cef174b0a395a902
Evelina Sabaityte <labasevelina@gmail.com>
2023-06-29 09:27:53 +0300

    implemented atlyginimoIrMokesciuFormules.ts calculations

--- src/utilities/atlyginimoIrMokesciuFormules.ts
+++ src/utilities/atlyginimoIrMokesciuFormules.ts
@@ -1,48 +1,37 @@
-class AtlyginimoIrMokesciuFormules {
-  calculateIncomeTax(grossSalary: number): number {
-    return grossSalary * 0.2; // 20% income tax
+const calculateIncomeTax = (grossSalary: number) => {
+  return grossSalary * 0.2; // 20% income tax
+};
+
+const calculateHealthInsuranceTax = (grossSalary: number) => {
+  return grossSalary * 0.0698; // 6.98 health insurance tax
+};
+
+const calculatePensionAndSocialSecurityTax = (grossSalary: number) => {
+  return grossSalary * 0.1252; // 12.52 pension and social security tax
+};
+
+const calculateSodraTax = (grossSalary: number) => {
+  return grossSalary * 0.0177; // 1.77 Sodra tax
+};
+
+const calculateFullCostOfWorkplace = (grossSalary: number) => {
+  return (
+    grossSalary +
+    calculateIncomeTax(grossSalary) +
+    calculateHealthInsuranceTax(grossSalary) +
+    calculatePensionAndSocialSecurityTax(grossSalary) +
+    calculateSodraTax(grossSalary)
+  );
+};
+
+const calculateNPD = (grossSalary: number) => {
+  if (grossSalary < 840) {
+    return 625; // NPD is 625 eu
+  } else if (grossSalary > 840 && grossSalary < 1926) {
+    return 625 - 0.42 * (grossSalary - 840);
+  } else if (grossSalary > 1926) {
+    return 400 - 0.18 * (grossSalary - 642);
   }
 
-  calculateHealthInsuranceTax(grossSalary: number): number {
-    return grossSalary * 0.0698; // 6.98 health insurance tax
-  }
-
-  calculatePensionAndSocialSecurityTax(grossSalary: number): number {
-    return grossSalary * 0.1252; // 12.52 pension and social security tax
-  }
-
-  calculateSodraTax(grossSalary: number): number {
-    return grossSalary * 0.0177; // 1.77 Sodra tax
-  }
-
-  calculateFullCostOfWorkplace(grossSalary: number): number {
-    return (
-      grossSalary +
-      this.calculateIncomeTax(grossSalary) +
-      this.calculateHealthInsuranceTax(grossSalary) +
-      this.calculatePensionAndSocialSecurityTax(grossSalary) +
-      this.calculateSodraTax(grossSalary)
-    );
-  }
-}
-
-let salaryCalculator = new AtlyginimoIrMokesciuFormules();
-
-let grossSalary = 10000; // example value
-
-console.log('Income Tax: ', salaryCalculator.calculateIncomeTax(grossSalary));
-console.log(
-  'Health Insurance Tax: ',
-  salaryCalculator.calculateHealthInsuranceTax(grossSalary)
-);
-console.log(
-  'Pension and Social Security Tax: ',
-  salaryCalculator.calculatePensionAndSocialSecurityTax(grossSalary)
-);
-console.log('Sodra Tax: ', salaryCalculator.calculateSodraTax(grossSalary));
-console.log(
-  'Full Cost of Workplace: ',
-  salaryCalculator.calculateFullCostOfWorkplace(grossSalary)
-);
-
-export default salaryCalculator;
+  return 0; // default return value if none of the conditions are met
+};
