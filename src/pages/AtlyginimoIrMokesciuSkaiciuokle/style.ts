559e825e08c79e2b65717fb5264c739e6bc5b5b3
Evelina Sabaityte <labasevelina@gmail.com>
2023-06-29 16:21:38 +0300

    added small changes

--- src/pages/AtlyginimoIrMokesciuSkaiciuokle/style.ts
+++ src/pages/AtlyginimoIrMokesciuSkaiciuokle/style.ts
@@ -1,37 +1,46 @@
 import styled from 'styled-components';
 
-export const StyledAtlyginimoMokesciuSkaiciuokle = styled.div`
+export const StyledAtlyginimoMokesciuSkaiciuokle = styled.section`
   display: flex;
   justify-content: center;
   align-items: start;
-  padding: 20px;
+  max-width: 1100px;
+
+  input {
+    &::placeholder {
+      color: #000;
+    }
+  }
 `;
 
-export const StyledAtlyginimoMokesciuSkaiciuokleContainer = styled.div`
+export const StyledSectionContainer = styled.div`
   text-align: center;
 `;
 
-export const StyledAtlyginimoMokesciuSkaiciuokleSubContainer = styled.div`
-  width: 100%;
+export const StyledBox = styled.div`
   display: flex;
   box-shadow: 0 0 15px -5px lightgray;
   border-radius: 15px;
-
-  > div {
-    height: 100%;
-    width: 50%;
-    padding: 2rem;
-    display: flex;
-
-    align-items: center;
-  }
 `;
+
+export const StyledBoxRight = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const StyledBoxLeft = styled.div`
+  background-color: rgb(248, 248, 248);
+  width: 100%;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const FormWrapper = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-bottom: 20px;
 `;
 
 export const ResultWrapper = styled.div`
@@ -39,5 +48,4 @@ export const ResultWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: start;
-  padding: 20px;
 `;
