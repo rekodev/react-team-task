fdd0e9e660b881885eaaa900f194ed55da5a5dd4
Simona <simajau@gmail.com>
2023-06-29 09:27:38 +0300

    created checkbox atom

--- src/components/Checkbox/style.ts
+++ src/components/Checkbox/style.ts
@@ -4,9 +4,12 @@ export const StyledCheckboxContainer = styled.div`
   display: flex;
   flex-direction: center;
   align-items: center;
-
   label {
-    color: #d9edf7;
+    color: black;
+
+    &:hover {
+      color: #3a87ad;
+    }
   }
   input {
     margin: 0.75rem;
