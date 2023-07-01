93c0e4f796ae6a5f62f88bc6ac9b957ef3005c8a
Simona <simajau@gmail.com>
2023-07-01 13:40:34 +0300

    styles-added

--- src/pages/ValiutuSkaiciuokle/style.ts
+++ src/pages/ValiutuSkaiciuokle/style.ts
@@ -11,3 +11,40 @@ export const StyledCurrencySelectList = styled.div`
   justify-content: center;
   align-items: start;
 `;
+
+export const StyledFlag = styled.img`
+  width: 1.25rem;
+  margin: 0.1rem;
+`;
+
+export const StyledInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledLabel = styled.label`
+  margin-right: 0.25rem;
+  margin-left: 1rem;
+`;
+
+export const StyledLabelFlagContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledButton = styled.button`
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+  margin-right: 1rem;
+
+  i {
+    color: #999999;
+    font-size: 20px;
+
+    &:hover {
+      color: #02d1b2;
+    }
+  }
+`;
