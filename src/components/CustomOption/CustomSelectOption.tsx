498a0782da8f0e33f1df77b3402fe66de83bde23
Simona <simajau@gmail.com>
2023-07-07 12:26:57 +0300

    removed customSelectOption - unused

--- src/components/CustomOption/CustomSelectOption.tsx
+++
@@ -1,8 +0,0 @@
-const CustomSelectOption = ({ value, children, arrowSvg }) => (
-  <option value={value}>
-    {children}
-    <span className='arrow-svg'>{arrowSvg}</span>
-  </option>
-);
-
-export default CustomSelectOption;
